64776c8413730ba4a15aedd25e198ed41b197334
Ganesh naik <142509529+ganeshlo@users.noreply.github.com>
2025-01-16 06:23:44 +0530

    Add files via upload

---
+++ src/main/java/spring/com/resource_allocation/App.java
@@ -0,0 +1,13 @@
+package spring.com.resource_allocation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
